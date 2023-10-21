43947e445015ee45f0df584ce98980df384f3259
BodleHG <20181588@edu.hanbat.ac.kr>
2023-10-21 22:58:27 +0900

    resize

--- 003 Code/Webrtc_Cam/offer.py
+++ 003 Code/Webrtc_Cam/offer.py
@@ -40,7 +40,7 @@ async def main():
             
             #해상도 줄여서 데이터 크기 축소(화질떨어짐)
             
-            frame = cv2.resize(frame,(1024, 720))  
+            frame = cv2.resize(frame,(640, 480))  
               
             if not ret :
                 break
